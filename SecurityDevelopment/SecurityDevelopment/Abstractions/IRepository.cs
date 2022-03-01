fbe5cf370d8df1079af60ca5049f4b6e23589ec4
Artur Stepanyants <alfacentaur@yandex.ru>
2022-03-01 19:10:40 +0300

    lesson2

---
+++ SecurityDevelopment/SecurityDevelopment/Abstractions/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SecurityDevelopment.Abstractions
+{
+    public interface IRepository
+    {
+        public JsonResult GetAll();
+
+        public JsonResult GetById(int Id);
+
+        public JsonResult GetByAnyStringValue(string AnyStringValue);
+
+        public JsonResult Create(object obj);
+
+        public JsonResult DeleteById(int Id);
+
+        public JsonResult Update(object obj);
+    }
+}
