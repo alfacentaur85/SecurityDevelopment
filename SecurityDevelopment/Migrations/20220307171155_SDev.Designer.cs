e717567c308104a865a53b21c1f3e0e98c83d246
Artur Stepanyants <alfacentaur@yandex.ru>
2022-04-03 18:23:28 +0300

    lesson4 mod 2

--- SecurityDevelopment/Migrations/20220307171155_SDev.Designer.cs
+++ SecurityDevelopment/Migrations/20220307171155_SDev.Designer.cs
@@ -3,13 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
-<<<<<<< HEAD
 
-=======
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
-using SecurityDevelopment;
->>>>>>> main
 
 namespace SecurityDevelopment.Migrations
 {
